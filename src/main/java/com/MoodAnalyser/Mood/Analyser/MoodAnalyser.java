9cf02889c45444e94fa0134cebd256c64312f2d7
Mayur <mayurzope555@gmail.com>
2020-09-30 23:02:19 +0530

    [Mayur]Added: Analyse and test happy mood or sad mood in Mood Analyser

--- src/main/java/com/MoodAnalyser/Mood/Analyser/MoodAnalyser.java
+++ src/main/java/com/MoodAnalyser/Mood/Analyser/MoodAnalyser.java
@@ -5,5 +5,11 @@ public class MoodAnalyser {
 	public void PrintWelcomeMessage() {
 		System.out.println("Welcome to the Mood Analyser Problem");
 	}
+	
+	public String AnalyseMood(String message) {
+		if(message.contains("Sad"))
+		return "SAD";
+		return "HAPPY";
+	}
 
 }
